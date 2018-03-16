7e39f1ff2ac5db219262953526058d61551c2877
Bala Chandran <balachandran.sivakumar@dciconsulting.co>
2018-03-16 12:00:05 +0530

    add index

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -8,7 +8,10 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-      
+      public ActionResult Index()
+        {
+
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
