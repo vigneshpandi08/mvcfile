8a437e9a72640d4154c1fcbd4b3b6cefb50b5159
Bala Chandran <balachandran.sivakumar@dciconsulting.co>
2018-03-16 11:56:13 +0530

    remove index

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -8,11 +8,7 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
+      
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
